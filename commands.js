97d5434084b86f1cab341d0df35dd82d36d3c9a0
jake <jakebrown536@gmail.com>
2022-12-05 18:42:22 -0700

    new quotes

--- commands.js
+++ commands.js
@@ -3,14 +3,14 @@
 
 // GETTING STARTED
 // 1. Create a database called jaskis
-'use jaskis'
+use jaskis
 
 // 2. Create a collection called bounties
-'db.createCollection(bounties)'
+db.createCollection('bounties')
 
 // ADD THE ANIMAL BOUNTIES
 // 1. Insert the given "Thanoceros" bounty object
-`db.jaskis.insertOne(
+db.jaskis.insertOne(
     {
     name: "Thanoceros",
     species: "Rhinoceros",
@@ -21,50 +21,50 @@
     captured: false
   }
     )
-`
+
 // 2. Query for all bounties in the bounties collection
-`db.jaskis.find()
-`
+db.jaskis.find()
+
 // MANAGE THE DATABASE
 // Queries
 // 1. Query for all bounties in the Grasslands
-`db.jaskis.find( {location: 'Grasslands'}
+db.jaskis.find( {location: 'Grasslands'}
 )
-`
+
 // 2. Query for all bounties with a reward worth 10000 or more
-`db.jaskis.find(
+db.jaskis.find(
     {reward: { $gt: 10000 }}
     )
-`
+
 // 3. Query for all bounties, but exclude the client attribute from being shown
-`db.jaskis.find(
+db.jaskis.find(
     {captured: false},{client: 0})
-`
+
 // 4. Query for a Groundhog in the Woodlands
-`db.jaskis.findOne(
+db.jaskis.findOne(
     {species: 'Groundhog'},{location: 'Woodlands'}
-    )
- `
+  )
+ 
 // Update and Delete
 // 1. Update the reward for Polarwind to 10000
-`db.jaskis.updateOne(
+db.jaskis.updateOne(
     {name: 'Polarwind'},{$set:{ reward: 10000}}
     )
-`
+
 // 2. Remove Lokinkajou
-`db.jaskis.deleteOne(
+db.jaskis.deleteOne(
     { name: 'Lokinkajou'}
     )
   { acknowledged: true, deletedCount: 1 }
-  `
+  
 // 3. Delete all bounties sent by Songbird
-`db.jaskis.deleteMany({
+db.jaskis.deleteMany({
     client: 'Songbird'}
     )
   { acknowledged: true, deletedCount: 3 }
-  `
+  
 // 4. Update all captured statuses to true
-`db.jaskis.updateMany({captured: false},{$set: {captured: true}})
+db.jaskis.updateMany({captured: false},{$set: {captured: true}})
 
 { 
   acknowledged: true,
@@ -72,4 +72,4 @@
   matchedCount: 3,
   modifiedCount: 3,
   upsertedCount: 0 
-}`
+}
